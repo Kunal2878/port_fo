39662e21c0260da7a4c1712f9e5f1bba59cb7fc1
Kunal <kunalpaul673@gmail.com>
2024-06-28 12:58:21 +0530

    changed

--- src/main.js
+++ src/main.js
@@ -3,30 +3,23 @@ import App from './App.vue'
 
 import { createApp } from 'vue'
 import { store } from './store'
-import cors from 'cors'
+
 // Plugins
 import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
-// const pinia = createPinia()
-
-// const Client_Id="148724204917-dpgah2sm4sdv5i9hqsbt5ceg8628h08o.apps.googleusercontent.com"
 registerPlugins(app)
-// app.use(vue3GoogleLogin, {
-//     clientId:"148724204917-dpgah2sm4sdv5i9hqsbt5ceg8628h08o.apps.googleusercontent.com",
-//     scope: 'email',
-//     prompt: 'select_account',
-//   })
+
   app.use(store);
-  // app.use(VueTelInput);
+
  
 
 
 // In your main.js file
 // import cors from 'cors';
 
-app.use(cors({
-  origin: true,
-}));
+// app.use(cors({
+//   origin: true,
+// }));
 
 app.mount('#app')
